0cae151474664d6a591fc366298421608db7ec56
rmortale <rmortale@dulce.ch>
2026-05-30 19:37:14 +0200

    added default cf

--- src/main/java/ch/dulce/camel/Templates.java
+++ src/main/java/ch/dulce/camel/Templates.java
@@ -10,6 +10,7 @@ public class Templates extends EndpointRouteBuilder {
 
   private static final String CACHE_LEVEL_NAME = "CACHE_CONSUMER";
   private static final String DEFAULT_MAX_CONSUMERS = "10";
+  private static final String DEFAULT_JMS_CONNECTION_FACTORY = "<default>";
 
   private static final String MESSAGE = """
       <object>
@@ -27,8 +28,8 @@ public class Templates extends EndpointRouteBuilder {
     routeTemplate("wrapbodyTransformation")
         .templateParameter("inqueue")
         .templateParameter("outqueue")
-        .templateParameter("inConnectionFactory", "<default>")
-        .templateParameter("outConnectionFactory", "<default>")
+        .templateParameter("inConnectionFactory", DEFAULT_JMS_CONNECTION_FACTORY)
+        .templateParameter("outConnectionFactory", DEFAULT_JMS_CONNECTION_FACTORY)
         .templateParameter("maxConsumers", DEFAULT_MAX_CONSUMERS)
         .from(jms("{{inqueue}}")
           .transacted(true)
@@ -42,29 +43,33 @@ public class Templates extends EndpointRouteBuilder {
     routeTemplate("uppercaseTransformation")
         .templateParameter("inqueue")
         .templateParameter("outqueue")
+        .templateParameter("inConnectionFactory", DEFAULT_JMS_CONNECTION_FACTORY)
+        .templateParameter("outConnectionFactory", DEFAULT_JMS_CONNECTION_FACTORY)
         .templateParameter("maxConsumers", DEFAULT_MAX_CONSUMERS)
         .from(jms("{{inqueue}}")
             .transacted(true)
             .cacheLevelName(CACHE_LEVEL_NAME)
-            .maxConcurrentConsumers("{{maxConsumers}}")
+            .maxConcurrentConsumers("{{maxConsumers}}").connectionFactory("{{inConnectionFactory}}")
             .advanced().lazyCreateTransactionManager(false))
         .transform().simple("${uppercase()}")
         .log(LoggingLevel.DEBUG, "uppercaseTransformation", "${body}")
-        .to(jms("{{outqueue}}"));
+        .to(jms("{{outqueue}}").connectionFactory("{{outConnectionFactory}}"));
 
     routeTemplate("extractBodyTransformation")
         .templateParameter("inqueue")
         .templateParameter("outqueue")
         .templateParameter("splitExpr")
+        .templateParameter("inConnectionFactory", DEFAULT_JMS_CONNECTION_FACTORY)
+        .templateParameter("outConnectionFactory", DEFAULT_JMS_CONNECTION_FACTORY)
         .templateParameter("maxConsumers", DEFAULT_MAX_CONSUMERS)
         .from(jms("{{inqueue}}")
             .transacted(true)
             .cacheLevelName(CACHE_LEVEL_NAME)
-            .maxConcurrentConsumers("{{maxConsumers}}")
+            .maxConcurrentConsumers("{{maxConsumers}}").connectionFactory("{{inConnectionFactory}}")
             .advanced().lazyCreateTransactionManager(false))
         .split().xtokenize("{{splitExpr}}", 'u', new Namespaces())
         .log(LoggingLevel.DEBUG, "extractBodyTransformation", "${body}")
-        .to(jms("{{outqueue}}"));
+        .to(jms("{{outqueue}}").connectionFactory("{{outConnectionFactory}}"));
   }
 
 }
