dfe2ddb303aedbdc4fddb098ad8d84cab6929c3c
rmortale <rmortale@dulce.ch>
2026-05-30 18:47:26 +0200

    added artemis secret

--- insert.sql
+++ insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO public.tpl_config(agentid, routeid, config_version, config_key, config_value)
-VALUES ('agent1', 'route123', '1.0', 'templateid', 'uppercaseTransformation');
+VALUES ('agent1', 'route123', '1.0', 'templateId', 'uppercaseTransformation');
 INSERT INTO public.tpl_config(agentid, routeid, config_version, config_key, config_value)
 VALUES ('agent1', 'route123', '1.0', 'inqueue', 'inqueue');
 INSERT INTO public.tpl_config(agentid, routeid, config_version, config_key, config_value)
 VALUES ('agent1', 'route123', '1.0', 'outqueue', 'outqueue');
 
 INSERT INTO public.tpl_config(agentid, routeid, config_version, config_key, config_value)
-VALUES ('agent1', 'route124', '1.0', 'templateid', 'wrapbodyTransformation');
+VALUES ('agent1', 'route124', '1.0', 'templateId', 'wrapbodyTransformation');
 INSERT INTO public.tpl_config(agentid, routeid, config_version, config_key, config_value)
 VALUES ('agent1', 'route124', '1.0', 'inqueue', 'inqueue');
 INSERT INTO public.tpl_config(agentid, routeid, config_version, config_key, config_value)
