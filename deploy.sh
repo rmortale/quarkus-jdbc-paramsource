696b0cf9773b287d00b328cefd7fd5bd6393a091
rmortale <rmortale@dulce.ch>
2026-08-02 16:48:43 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEPLOY_NAMESPACE=${1:-devt}
+DEPLOY_TAG=${2:-main}
+DEPLOY_ACTION=${3:-deploy}
+
+git checkout $DEPLOY_TAG
+kubens $DEPLOY_NAMESPACE
+
+if [[ "$DEPLOY_ACTION" == "undeploy" ]]; then
+  printf '\nUNDEPLOY environment %s...\n\n' "$DEPLOY_NAMESPACE"
+  kubectl delete -k k8s/overlays/$DEPLOY_NAMESPACE
+else
+  printf '\nDeploying tag %s to environment %s...\n\n' "$DEPLOY_TAG" "$DEPLOY_NAMESPACE"
+  kubectl apply -k k8s/overlays/$DEPLOY_NAMESPACE
+fi
+
+
+
+
+
+
